e8f040560e0e77fd40ab39f6b39a1c7cf0ec68bf
sungwoo-pasqal <sungwoo.ahn@pasqal.com>
2025-04-07 17:38:03 +0200

    lint

--- qadence-measurements/qadence_measurements/types.py
+++ qadence-measurements/qadence_measurements/types.py
@@ -9,14 +9,13 @@ import torch
 from torch import Tensor
 
 
-
-
 @dataclass
 class MeasurementData:
     samples: Tensor | list[Counter] = dataclass_field(default_factory=list)
     """Samples from protocol."""
     unitaries: Tensor = torch.empty(0)
     """Random unitaries used in shadows."""
+
 
 class MeasurementProtocol(StrEnum):
     TOMOGRAPHY = "tomography"
@@ -25,6 +24,3 @@ class MeasurementProtocol(StrEnum):
     """Snapshots of a state via shadows."""
     ROBUST_SHADOW = "robust_shadow"
     """Snapshots of a state via shadows for noisy settings."""
-
-
-
